54af005e9936dde8af70a2503650b4ea708e3022
s.ohta <s.ohta@qibitech.com>
2022-08-29 20:44:27 +0900

    impedance control sample program

--- amber_service_examples/src/amber_service_examples/amber_impedance_control.py
+++ amber_service_examples/src/amber_service_examples/amber_impedance_control.py
@@ -17,18 +17,18 @@ from amber_ros_driver.srv import (
 
 class AmberImpedanceControl(object):
 
-    _srv_setcontrolmode = '/set_control_mode'
-    _srv_servoallon = '/servo_all_on'
-    _srv_servoon = '/servo_on'
-    _srv_setjointtrajectory = '/set_joint_trajectory'
-    _srv_waitinterpolation = '/wait_interpolation'
-    _srv_servoalloff = '/servo_all_off'
-    _srv_servooff = '/servo_off'
-    _srv_restposition = '/go_to_rest_position'
+    _SRV_SETCONTROLMODE = '/set_control_mode'
+    _SRV_SERVOALLON = '/servo_all_on'
+    _SRV_SERVOON = '/servo_on'
+    _SRV_SETJOINTTRAJECTORY = '/set_joint_trajectory'
+    _SRV_WAITINTERPOLATION = '/wait_interpolation'
+    _SRV_SERVOALLOFF = '/servo_all_off'
+    _SRV_SERVOOFF = '/servo_off'
+    _SRV_RESETPOSITION = '/go_to_rest_position'
 
     def __init__(self):
 
-        lr = self._LR = rospy.get_param('~amber_lr', default='amber_right')
+        self._LR = rospy.get_param('~amber_lr', default='amber_right')
         self._IMPD_P_GAIN = float(rospy.get_param('pgain', default=-2))
         self._IMPD_D_GAIN = float(rospy.get_param('dgain', default=-3))
 
@@ -46,84 +46,84 @@ class AmberImpedanceControl(object):
         srv_dict = {}
 
         # set control mode
-        srv_name = lr+self._srv_setcontrolmode
+        srv_name = self._LR+self._SRV_SETCONTROLMODE
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_setcontrolmode] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SETCONTROLMODE] = rospy.ServiceProxy(
             srv_name,
             SetInt8Array
         )
 
         # servo all on
-        srv_name = lr+self._srv_servoallon
+        srv_name = self._LR+self._SRV_SERVOALLON
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_servoallon] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SERVOALLON] = rospy.ServiceProxy(
             srv_name,
             Empty
         )
 
         # servo on
-        srv_name = lr+self._srv_servoon
+        srv_name = self._LR+self._SRV_SERVOON
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_servoon] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SERVOON] = rospy.ServiceProxy(
             srv_name,
             SetJointNo
         )
 
         # set joint trajectory
-        srv_name = lr+self._srv_setjointtrajectory
+        srv_name = self._LR+self._SRV_SETJOINTTRAJECTORY
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_setjointtrajectory] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SETJOINTTRAJECTORY] = rospy.ServiceProxy(
             srv_name,
             SetJointTrajectory
         )
 
         # wait interpolation
-        srv_name = lr+self._srv_waitinterpolation
+        srv_name = self._LR+self._SRV_WAITINTERPOLATION
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_waitinterpolation] = rospy.ServiceProxy(
+        srv_dict[self._SRV_WAITINTERPOLATION] = rospy.ServiceProxy(
             srv_name,
             Empty
         )
 
         # servo all off
-        srv_name = lr+self._srv_servoalloff
+        srv_name = self._LR+self._SRV_SERVOALLOFF
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_servoalloff] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SERVOALLOFF] = rospy.ServiceProxy(
             srv_name,
             Empty
         )
 
         # servo off
-        srv_name = lr+self._srv_servooff
+        srv_name = self._LR+self._SRV_SERVOOFF
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_servooff] = rospy.ServiceProxy(
+        srv_dict[self._SRV_SERVOOFF] = rospy.ServiceProxy(
             srv_name,
             SetJointNo
         )
 
         # go to rest position
-        srv_name = lr+self._srv_restposition
+        srv_name = self._LR+self._SRV_RESETPOSITION
         rospy.wait_for_service(srv_name)
-        srv_dict[self._srv_restposition] = rospy.ServiceProxy(
+        srv_dict[self._SRV_RESETPOSITION] = rospy.ServiceProxy(
             srv_name,
             Empty
         )
 
-        self._SERVICE_DICT[lr] = srv_dict
+        self._SERVICE_DICT[self._LR] = srv_dict
 
         self._SUBSCRIBERS.append(
-            rospy.Subscriber(lr+'/joint_currents',
+            rospy.Subscriber(self._LR+'/joint_currents',
                              Float64MultiArray,
                              self._joint_currents_cb,
                              queue_size=1,
-                             callback_args=lr)
+                             callback_args=self._LR)
         )
         self._SUBSCRIBERS.append(
-            rospy.Subscriber(lr+'/joint_states',
+            rospy.Subscriber(self._LR+'/joint_states',
                              JointState,
                              self._joint_states_cb,
                              queue_size=1,
-                             callback_args=lr)
+                             callback_args=self._LR)
         )
         self._SUBSCRIBERS.append(
             rospy.Subscriber('scenario_key',
@@ -174,7 +174,7 @@ class AmberImpedanceControl(object):
                                   close_offset=0.3,
                                   contact_current=0.5):
         start_currents = self._get_current_average(1.0)
-        self._SERVICE_DICT[lr][self._srv_setjointtrajectory](
+        self._SERVICE_DICT[lr][self._SRV_SETJOINTTRAJECTORY](
             [0, 0, 0, 0, 0, close_angle, close_angle],
             close_time,
             [1, 1, 1, 1, 1, 0, 0],
@@ -182,115 +182,67 @@ class AmberImpedanceControl(object):
             False
         )
         diff_c = [i-j for i,j in zip(self._CURRENTS_DATA[lr], start_currents[lr])]
-        # diff_c = self._CURRENTS_DATA[lr]
         while (abs(diff_c[5]) < contact_current \
                or abs(diff_c[6]) < contact_current) \
                and not rospy.is_shutdown():
-            diff_c = self._CURRENTS_DATA[lr]
-            print(diff_c[5:])
+            diff_c = [i-j for i,j in zip(self._CURRENTS_DATA[lr], start_currents[lr])]
             time.sleep(0.01)
-        self._SERVICE_DICT[lr][self._srv_setjointtrajectory](
+        self._SERVICE_DICT[lr][self._SRV_SETJOINTTRAJECTORY](
             [0, 0, 0, 0, 0, 0, 0],
             0.01,
             [1, 1, 1, 1, 1, 0, 0],
             self._INTP_MINJERK,
             True
         )
-        self._SERVICE_DICT[lr][self._srv_waitinterpolation]()
+        self._SERVICE_DICT[lr][self._SRV_WAITINTERPOLATION]()
         time.sleep(1)
-        rospy.loginfo('grasp offset')
-        self._SERVICE_DICT[lr][self._srv_setjointtrajectory](
+        rospy.loginfo('-- grasp offset')
+        self._SERVICE_DICT[lr][self._SRV_SETJOINTTRAJECTORY](
             [0, 0, 0, 0, 0, close_offset, close_offset],
             1.0,
             [1, 1, 1, 1, 1, 0, 0],
             self._INTP_MINJERK,
             True
         )
-        self._SERVICE_DICT[lr][self._srv_waitinterpolation]()
-
-
-    def close_hand_test(self):
-        rospy.loginfo("- control mode: POSITION mode")
-        self._SERVICE_DICT[self._LR][self._srv_setcontrolmode]([1, 1, 1, 1, 1, 1, 1])
-        time.sleep(0.2)
-
-        # servo all on
-        rospy.loginfo("- servo ALL ON")
-        self._SERVICE_DICT[self._LR][self._srv_servoallon]()
-        time.sleep(0.5)
-
-        # set joint trajectory by position mode
-        rospy.loginfo("- joint trajectory")
-        self._SERVICE_DICT[self._LR][self._srv_setjointtrajectory](
-            [0.0, -0.055, -1.57, 0.0, 1.57, -0.5, -0.5],
-            3.0,
-            self._NOMASK,
-            self._INTP_MINJERK,
-            False
-        )
-
-        # wait interpolation
-        rospy.loginfo("- wait interpolation...")
-        self._SERVICE_DICT[self._LR][self._srv_waitinterpolation]()
-        rospy.loginfo("- reached")
-
-        # set plate
-        rospy.loginfo("- ")
-        rospy.loginfo('After setting plate on hand, please press the "Enter" key')
-        self._wait_key_enter()
-
-        # grasp
-        self._close_hand_until_contact(self._LR)
+        self._SERVICE_DICT[lr][self._SRV_WAITINTERPOLATION]()
 
 
     def _impedance_control(self, event):
-        lr = self._LR
         if self._IMPD_ACTIVE:
-            pos_diff = [i-j for i,j in zip(self._POSITIONS_DATA[lr], self._START_POSITIONS[lr])]
-            pos_dd = [i-j for i,j in zip(pos_diff, self._PRE_POS_DIFF[lr])]
-            target_currents = [0]*len(self._START_CURRENTS[lr])
-            target_currents[5] = self._grasp_angle
-            target_currents[6] = self._grasp_angle
+            pos_diff = [i-j for i,j in zip(self._POSITIONS_DATA[self._LR],
+                                           self._START_POSITIONS[self._LR])]
+            pos_dd = [i-j for i,j in zip(pos_diff, self._PRE_POS_DIFF[self._LR])]
+            target_currents = [0]*len(self._START_CURRENTS[self._LR])
             for i in range(3):
-                target_currents[i] = self._START_CURRENTS[lr][i] \
+                target_currents[i] = self._START_CURRENTS[self._LR][i] \
                     + pos_diff[i]*self._IMPD_P_GAIN + pos_dd[i]*self._IMPD_D_GAIN
-            rospy.loginfo(target_currents)
-            self._SERVICE_DICT[lr][self._srv_setjointtrajectory](
+            rospy.logdebug(target_currents)
+            self._SERVICE_DICT[self._LR][self._SRV_SETJOINTTRAJECTORY](
                 target_currents,
                 0.01,
                 [0, 0, 0, 1, 1, 1, 1],
                 self._INTP_MINJERK,
                 False
             )
-            self._PRE_POS_DIFF[lr] = pos_diff
-
-
-    def test(self):
-        rospy.loginfo("sceen1")
-        self._wait_key_enter()
-        rospy.loginfo("sceen2")
-        self._wait_key_enter()
-        rospy.loginfo("sceen3")
-        self._wait_key_enter()
-        rospy.loginfo("sceen4")
-        self._wait_key_enter()
+            self._PRE_POS_DIFF[self._LR] = pos_diff
 
 
     def run(self):
         # set control mode to position
         rospy.loginfo("- control mode: POSITION mode")
-        self._SERVICE_DICT[self._LR][self._srv_setcontrolmode]([1, 1, 1, 1, 1, 1, 1])
-        time.sleep(0.2)
+        self._SERVICE_DICT[self._LR][self._SRV_SETCONTROLMODE]([1, 1, 1, 1, 1, 1, 1])
+        time.sleep(1)
+        rospy.loginfo("ok")
 
         # servo all on
         rospy.loginfo("- servo ALL ON")
-        self._SERVICE_DICT[self._LR][self._srv_servoallon]()
-        # self._SERVICE_DICT[self._LR][self._srv_servoon](0)
-        time.sleep(0.5)
+        self._SERVICE_DICT[self._LR][self._SRV_SERVOALLON]()
+        time.sleep(1)
+        rospy.loginfo("ok")
 
         # set joint trajectory by position mode
         rospy.loginfo("- joint trajectory")
-        self._SERVICE_DICT[self._LR][self._srv_setjointtrajectory](
+        self._SERVICE_DICT[self._LR][self._SRV_SETJOINTTRAJECTORY](
             [0.0, -0.055, -1.57, 0.0, 1.57, -0.5, -0.5],
             3.0,
             self._NOMASK,
@@ -300,69 +252,51 @@ class AmberImpedanceControl(object):
 
         # wait interpolation
         rospy.loginfo("- wait interpolation...")
-        self._SERVICE_DICT[self._LR][self._srv_waitinterpolation]()
-        rospy.loginfo("- reached")
+        self._SERVICE_DICT[self._LR][self._SRV_WAITINTERPOLATION]()
+        rospy.loginfo("ok")
 
         # set plate
-        rospy.loginfo("- ")
+        rospy.loginfo('***')
         rospy.loginfo('After setting plate on hand, please press the "Enter" key')
         self._wait_key_enter()
 
         # set joint trajectory by position mode
         rospy.loginfo("- graspping...")
-        # self._close_hand_until_contact(self._LR)
-        self._grasp_angle = 0.3
-        self._SERVICE_DICT[self._LR][self._srv_setjointtrajectory](
-            [0.0, -0.055, -1.57, 0.0, 1.57, self._grasp_angle, self._grasp_angle],
-            3.0,
-            self._NOMASK,
-            self._INTP_MINJERK,
-            False
-        )
-        rospy.loginfo("- wait interpolation...")
-        self._SERVICE_DICT[self._LR][self._srv_waitinterpolation]()
-        rospy.loginfo("- grasp")
+        self._close_hand_until_contact(self._LR)
+        rospy.loginfo("grasp")
 
         # get now sensor data
         rospy.loginfo("- get data...")
         time.sleep(1)
         self._START_CURRENTS = self._get_current_average(2)
-        print(self._START_CURRENTS[self._LR])
         self._START_POSITIONS = self._POSITIONS_DATA.copy()
         self._PRE_POS_DIFF = self._START_POSITIONS.copy()
         for k,v in self._PRE_POS_DIFF.items():
             self._PRE_POS_DIFF[k] = [0]*len(v)
-        rospy.loginfo("- ok")
-
-        # wait input enter key
-        rospy.loginfo("- ")
-        rospy.loginfo("After grabbing the plate, please press the enter key")
-        self._wait_key_enter()
+        rospy.loginfo("ok")
 
         # change control mode to current, then set joint trajectory
         rospy.loginfo("- change control mode to CURRENT mode from joint 0 to 2")
-        self._START_CURRENTS[5] = self._grasp_angle
-        self._START_CURRENTS[6] = self._grasp_angle
-        self._SERVICE_DICT[self._LR][self._srv_setcontrolmode]([3, 3, 3, 1, 1, 1, 1])
-        self._SERVICE_DICT[self._LR][self._srv_setjointtrajectory](
+        self._SERVICE_DICT[self._LR][self._SRV_SETCONTROLMODE]([3, 3, 3, 1, 1, 1, 1])
+        self._SERVICE_DICT[self._LR][self._SRV_SETJOINTTRAJECTORY](
             self._START_CURRENTS[self._LR],
             0.1,
             [0, 0, 0, 1, 1, 1, 1],
             self._INTP_MINJERK,
             False
         )
-        self._SERVICE_DICT[self._LR][self._srv_waitinterpolation]()
-        rospy.loginfo("- ok")
+        self._SERVICE_DICT[self._LR][self._SRV_WAITINTERPOLATION]()
+        rospy.loginfo("ok")
 
         # start impedance control
         self._IMPD_ACTIVE = True
         impd_timers = []
         impd_timers.append(rospy.Timer(rospy.Duration(0.1), self._impedance_control))
-        rospy.loginfo("- start impedance control")
+        rospy.loginfo("- <<< Start impedance control >>>")
 
         # wait input enter key
-        rospy.loginfo("- ")
-        rospy.loginfo("finish by pressing the enter key")
+        rospy.loginfo('***')
+        rospy.loginfo("Finish by pressing the enter key")
         self._wait_key_enter()
         self._IMPD_ACTIVE = False
         for it in impd_timers:
@@ -371,21 +305,34 @@ class AmberImpedanceControl(object):
 
         # change control mode to position, then set joint trajectory
         rospy.loginfo("- change control mode to POSITION mode from joint 0 to 2")
-        self._SERVICE_DICT[self._LR][self._srv_servooff](2)
-        time.sleep(0.1)
-        self._SERVICE_DICT[self._LR][self._srv_setcontrolmode]([1, 1, 1, 1, 1, 1, 1])
-        time.sleep(0.1)
-        self._SERVICE_DICT[self._LR][self._srv_servoon](2)
-        time.sleep(0.1)
-        rospy.loginfo("- ok")
+        self._SERVICE_DICT[self._LR][self._SRV_SETCONTROLMODE]([1, 1, 1, 1, 1, 1, 1])
+        time.sleep(1)
+        rospy.loginfo("ok")
+
+        # release plate
+        rospy.loginfo("- open hand")
+        self._SERVICE_DICT[self._LR][self._SRV_SETJOINTTRAJECTORY](
+            [0, 0, 0, 0, 0, -0.5, -0.5],
+            3.0,
+            [1,1,1,1,1,0,0],
+            self._INTP_MINJERK,
+            False
+        )
+        self._SERVICE_DICT[self._LR][self._SRV_WAITINTERPOLATION]()
+        rospy.loginfo("ok")
 
         # rest position
         rospy.loginfo("- move rest position")
-        self._SERVICE_DICT[self._LR][self._srv_restposition]()
+        self._SERVICE_DICT[self._LR][self._SRV_RESETPOSITION]()
+        rospy.loginfo("ok")
 
         # servo all off
         rospy.loginfo("- servo ALL OFF")
-        self._SERVICE_DICT[self._LR][self._srv_servoalloff]()
+        self._SERVICE_DICT[self._LR][self._SRV_SERVOALLOFF]()
+        rospy.loginfo("ok")
+
+        rospy.loginfo("*** Demo finish ***")
+        rospy.loginfo("Please 'Ctrl-c' and press 'Enter'")
 
 
 
@@ -397,6 +344,9 @@ class AmberImpedanceControlDemoScenario(object):
         input('')
 
     def run(self):
-        while not rospy.is_shutdown():
-            self._wait_key_enter()
-            self._pub.publish()
+        try:
+            while not rospy.is_shutdown():
+                self._wait_key_enter()
+                self._pub.publish()
+        finally:
+            rospy.loginfo("<< Please press 'Enter' to exit >>")
